bd575114348d0ab915fe6161a8ae6a9570c501a9
Nikki <bravonikki13@gmail.com>
2026-07-30 11:14:50 +0200

    fix: add unoptimized attribute to banner because it's not updating

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -37,6 +37,7 @@ export function Hero() {
           alt='Cute pastel banner'
           fill
           priority
+          unoptimized
           fetchPriority='high'
           className='object-cover'
         />
